648a5fc1fb3f0941af82955953bd5d3e8d7f07f0
Owarimasu <401205991@qq.com>
2021-06-12 21:23:19 +0800

    Click
    Click Event

---
+++ Assets/script/chair.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chair : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        Debug.Log("OnMouseDown");
+    }
+
+}
---
+++ Assets/script/magazine.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class magazine : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        Debug.Log("OnMouseDownMagazine");
+    }
+
+}
