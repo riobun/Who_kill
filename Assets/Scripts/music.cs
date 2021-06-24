4696e3408652d662d867b5c2b7b476b4d6d173ea
riobun <1712418057@qq.com>
2021-06-24 18:06:30 +0800

    场景修改，添加音乐

---
+++ Assets/Scripts/music.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class music : MonoBehaviour
+{
+    static music _instance;
+    AudioSource Orange;//获取AudioOrange组件
+
+    public AudioClip audio1;
+
+    private Scene scene;
+    private bool play = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Orange = transform.GetComponent<AudioSource>();
+        this.scene = SceneManager.GetActiveScene();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.scene = SceneManager.GetActiveScene();
+        if (this.scene.buildIndex == 0)
+
+        {
+            if (play == true)
+                return;
+
+            Orange.Stop();
+            Orange.clip = audio1;
+            Orange.Play();
+            play = true;
+        }
+    }
+
+    public static music instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<music>();
+                DontDestroyOnLoad(_instance.gameObject);
+            }
+            return _instance;
+        }
+    }
+
+    void Awake()
+    {
+
+        //此脚本永不消毁，并且每次进入初始场景时进行判断，若存在重复的则销毁
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else if (this != _instance)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
